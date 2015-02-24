8c5d73ba14ed74d5cf8607194f14a5305d7af054
jtimberman <joshua@chef.io>
2015-02-23 21:50:39 -0700

    add CentOS 5.11 to test matrix
    This commit adds CentOS 5.11 to the test matrix. This requires working
    around a bug in the bento box where the cacert.pem bundle doesn’t have
    the VeriSign certificates that AWS uses.
    
    https://github.com/chef/bento/issues/325
    
    This workaround is applied as a recipe in the test cookbook because it
    may not be necessary for end users who may not have that certificate
    bundle.

--- test/fixtures/cookbooks/test/recipes/default.rb
+++ test/fixtures/cookbooks/test/recipes/default.rb
@@ -7,6 +7,12 @@
 # is run.
 execute 'apt-get update' if platform_family?('debian')
 
+remote_file '/etc/pki/tls/certs/ca-bundle.crt' do
+  source 'http://opscode-omnibus-cache.s3.amazonaws.com/cacerts-2014.07.15-fd48275847fa10a8007008379ee902f1'
+  checksum 'a9cce49cec92304d29d05794c9b576899d8a285659b3f987dd7ed784ab3e0621'
+  sensitive true
+end if platform_family?('rhel')
+
 chef_server_ingredient 'chef-server-core' do
   action [:install, :reconfigure]
   version node['test']['chef-server-core']['version']
