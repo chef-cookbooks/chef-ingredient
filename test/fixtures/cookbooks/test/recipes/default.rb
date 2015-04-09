d36e7b6dd23a82a84cb20ba5ece09c3be80a7522
Sean OMeara <sean@chef.io>
2015-04-09 00:37:19 -0400

    moving apt updating to kitchen.yml

--- test/fixtures/cookbooks/test/recipes/default.rb
+++ test/fixtures/cookbooks/test/recipes/default.rb
@@ -1,5 +1,3 @@
-execute 'apt-get update' if platform_family?('debian')
-
 remote_file '/etc/pki/tls/certs/ca-bundle.crt' do
   source 'http://opscode-omnibus-cache.s3.amazonaws.com/cacerts-2014.07.15-fd48275847fa10a8007008379ee902f1'
   checksum 'a9cce49cec92304d29d05794c9b576899d8a285659b3f987dd7ed784ab3e0621'
