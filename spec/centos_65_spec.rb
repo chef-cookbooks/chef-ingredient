7c1dafdb9b7b4d8ace7ab916277ad81d6ab991f7
Irving Popovetsky <irving@getchef.com>
2015-06-04 16:49:42 -0700

    Add tests for the test::local recipe and verify that the right package provider is used. also get to 100% test coverage

--- spec/centos_65_spec.rb
+++ spec/centos_65_spec.rb
@@ -4,15 +4,24 @@ describe 'test::repo' do
   cached(:centos_65) do
     ChefSpec::SoloRunner.new(
       platform: 'centos',
-      version: '6.5'
+      version: '6.5',
+      step_into: 'chef_server_ingredient'
       ) do |node|
       node.set['chef-server-core']['version'] = nil
     end.converge('test::repo')
   end
 
   context 'compiling the test recipe' do
+    it 'creates packagecloud_repo[chef/stable]' do
+      expect(centos_65).to create_packagecloud_repo('chef/stable')
+    end
+
     it 'installs chef_server_ingredient[chef-server-core]' do
       expect(centos_65).to install_chef_server_ingredient('chef-server-core')
+    end
+
+    it 'installs yum_package[chef-server-core]' do
+      expect(centos_65).to install_yum_package('chef-server-core')
     end
 
     it 'creates file[/tmp/chef-server-core.firstrun]' do
@@ -23,8 +32,40 @@ describe 'test::repo' do
       expect(centos_65).to install_chef_server_ingredient('opscode-manage')
     end
 
+    it 'installs yum_package[opscode-manage]' do
+      expect(centos_65).to install_yum_package('opscode-manage')
+    end
+
     it 'creates file[/tmp/opscode-manage.firstrun]' do
       expect(centos_65).to create_file('/tmp/opscode-manage.firstrun')
+    end
+  end
+end
+
+
+describe 'test::local' do
+  cached(:centos_65) do
+    ChefSpec::SoloRunner.new(
+      platform: 'centos',
+      version: '6.5',
+      step_into: 'chef_server_ingredient'
+      ) do |node|
+      node.set['chef-server-core']['version'] = nil
+    end.converge('test::local')
+  end
+
+  context 'compiling the test recipe' do
+    it 'installs chef_server_ingredient[chef-server-core]' do
+      expect(centos_65).to install_chef_server_ingredient('chef-server-core')
+    end
+
+    it 'uses the rpm_package provider instead of yum_package' do
+      expect(centos_65).to install_rpm_package('chef-server-core')
+      expect(centos_65).to_not install_yum_package('chef-server-core')
+    end
+
+    it 'creates file[/tmp/chef-server-core.firstrun]' do
+      expect(centos_65).to create_file('/tmp/chef-server-core.firstrun')
     end
   end
 end
--- spec/ubuntu_1404_spec.rb
+++ spec/ubuntu_1404_spec.rb
@@ -4,15 +4,24 @@ describe 'test::repo' do
   cached(:ubuntu_1404) do
     ChefSpec::SoloRunner.new(
       platform: 'ubuntu',
-      version: '14.04'
+      version: '14.04',
+      step_into: 'chef_server_ingredient'
       ) do |node|
       node.set['chef-server-core']['version'] = nil
     end.converge('test::repo')
   end
 
   context 'compiling the test recipe' do
+    it 'creates packagecloud_repo[chef/stable]' do
+      expect(ubuntu_1404).to create_packagecloud_repo('chef/stable')
+    end
+
     it 'installs chef_server_ingredient[chef-server-core]' do
       expect(ubuntu_1404).to install_chef_server_ingredient('chef-server-core')
+    end
+
+    it 'installs apt_package[chef-server-core]' do
+      expect(ubuntu_1404).to install_apt_package('chef-server-core')
     end
 
     it 'creates file[/tmp/chef-server-core.firstrun]' do
@@ -23,8 +32,39 @@ describe 'test::repo' do
       expect(ubuntu_1404).to install_chef_server_ingredient('opscode-manage')
     end
 
+    it 'installs apt_package[opscode-manage]' do
+      expect(ubuntu_1404).to install_apt_package('opscode-manage')
+    end
+
     it 'creates file[/tmp/opscode-manage.firstrun]' do
       expect(ubuntu_1404).to create_file('/tmp/opscode-manage.firstrun')
+    end
+  end
+end
+
+describe 'test::local' do
+  cached(:ubuntu_1404) do
+    ChefSpec::SoloRunner.new(
+      platform: 'ubuntu',
+      version: '14.04',
+      step_into: 'chef_server_ingredient'
+      ) do |node|
+      node.set['chef-server-core']['version'] = nil
+    end.converge('test::local')
+  end
+
+  context 'compiling the test recipe' do
+    it 'installs chef_server_ingredient[chef-server-core]' do
+      expect(ubuntu_1404).to install_chef_server_ingredient('chef-server-core')
+    end
+
+    it 'uses the rpm_package provider instead of yum_package' do
+      expect(ubuntu_1404).to install_dpkg_package('chef-server-core')
+      expect(ubuntu_1404).to_not install_apt_package('chef-server-core')
+    end
+
+    it 'creates file[/tmp/chef-server-core.firstrun]' do
+      expect(ubuntu_1404).to create_file('/tmp/chef-server-core.firstrun')
     end
   end
 end
