85db842a9b2bfeb147837b6d231655c0bf22e913
Irving Popovetsky <irving@getchef.com>
2015-06-10 08:36:23 -0700

    Add a fallback for local_package_resource if no platforms match

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -42,6 +42,7 @@ module ChefServerIngredientsCookbook
     def local_package_resource
       return :dpkg_package if node['platform_family'] == 'debian'
       return :rpm_package if node['platform_family'] == 'rhel'
+      :package # fallback if there's no platform match
     end
 
     def ctl_command
