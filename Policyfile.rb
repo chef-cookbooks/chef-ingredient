55040cf2aa417cf6b883896d397d290aedab266d
jtimberman <joshua@chef.io>
2015-09-16 15:30:32 -0700

    use policyfiles for specs instead of berkshelf

---
+++ Policyfile.rb
@@ -0,0 +1,9 @@
+name 'chef-ingredient'
+run_list 'chef-ingredient::default'
+default_source :community
+
+cookbook 'chef-ingredient',
+  path: '.'
+
+cookbook 'test',
+  path: './test/fixtures/cookbooks/test'
