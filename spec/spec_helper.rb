77fdc00df41b4aec650bf59cea229d7aa38a5dfe
Salim Afiune <afiune@chef.io>
2015-10-07 19:55:21 -0500

    Add Libraries for testing and comment policyfiles
    For now we haven't released `policyfiles` and the `Berksfile` was re
    introduced so we are going to comment it for now till we are ready to
    use it.
    
    Added Libraries for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,14 @@
 require 'chefspec'
+require 'chefspec/berkshelf'
 
 # Requires https://github.com/sethvargo/chefspec/commit/cd57e28fdbd59fc26962c0dd3b1809b8841312f3
-require 'chefspec/policyfile'
+# require 'chefspec/policyfile'
+
+TOPDIR = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+SUPPORT_DIR = File.join(TOPDIR, 'spec', 'support')
+$LOAD_PATH << File.expand_path(File.dirname(__FILE__))
+
+# Require all our libraries
+Dir['libraries/*.rb'].each { |f| require File.expand_path(f) }
 
 at_exit { ChefSpec::Coverage.report! }
