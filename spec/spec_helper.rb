ca1fde16246587cc08541fc509f65925b312091d
jtimberman <joshua@chef.io>
2015-10-17 14:19:42 -0600

    Remove unnecessary tests for chef_ingredient
    These tests are not high value, requiring future maintenance that we don't feel is necessary. The functionality here is largely testing that Chef's resource and provider model works, which we don't need to do.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,11 +7,4 @@ RSpec.configure do |config|
   config.log_level = :error
 end
 
-TOPDIR = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-SUPPORT_DIR = File.join(TOPDIR, 'spec', 'support')
-$LOAD_PATH << File.expand_path(File.dirname(__FILE__))
-
-# Require all our libraries
-Dir['libraries/*.rb'].each { |f| require File.expand_path(f) }
-
 at_exit { ChefSpec::Coverage.report! }
