0fcad63b1f975cfa5db835c17180ca4ce56aa34a
Patrick Wright <patrick@getchef.com>
2016-04-18 11:58:34 -0700

    add kitchen comliance test

--- test/fixtures/cookbooks/test/recipes/compliance.rb
+++ test/fixtures/cookbooks/test/recipes/compliance.rb
@@ -3,13 +3,6 @@
 # This channel is only accessible from Chef's internal
 # network and contains untested builds of Chef products.
 #
-# Make sure you set below environment variables for
-# tests to work correctly.
-#
-
-include_recipe 'apt'
-include_recipe 'yum'
-include_recipe 'git'
 
 chef_ingredient 'compliance' do
   action :install
