5c60720f3ab28e94acc5c4e6b24e436701cc78e8
Tim Smith <tsmith@chef.io>
2016-07-29 16:19:53 -0700

    Remove Chef 11 compat in the metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,5 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Primitives for managing Chef products and packages'
 
-source_url 'https://github.com/chef-cookbooks/chef-ingredient' if defined?(:source_url)
-issues_url 'https://github.com/chef-cookbooks/chef-ingredient/issues' if defined?(:issues_url)
+source_url 'https://github.com/chef-cookbooks/chef-ingredient'
+issues_url 'https://github.com/chef-cookbooks/chef-ingredient/issues'
