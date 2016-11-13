e05fab00118196dff8393f36261300764182a5c1
Nathan Cerny <ncerny@gmail.com>
2016-11-13 08:12:09 -0600

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'chef'
+maintainer 'Chef Software Inc'
+maintainer_email 'success@chef.io'
+license 'apachev2'
+description 'Installs/Configures chef'
+long_description 'Installs/Configures chef'
+version '0.1.0'
