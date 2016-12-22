f6b9c28eaddee542047f453389c7bded070b4777
Tim Smith <tsmith@chef.io>
2016-12-22 09:33:32 -0800

    Depend on a working compat_resource
    We just resolved some issues that led to failures on older chef-client releases. We should depend on the working version now.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Primitives for managing Chef products and packages'
 
-depends 'compat_resource', '>= 12.10'
+depends 'compat_resource', '>= 12.16.3'
 
 source_url 'https://github.com/chef-cookbooks/chef-ingredient'
 issues_url 'https://github.com/chef-cookbooks/chef-ingredient/issues'
