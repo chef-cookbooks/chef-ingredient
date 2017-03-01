170650c40f43ebc2b708e70f08f014c1b614dd65
Tim Smith <tsmith@chef.io>
2017-03-01 10:57:27 -0800

    Test in Travis CI with kitchen-dokken
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -7,5 +7,4 @@ description 'Primitives for managing Chef products and packages'
 
 source_url 'https://github.com/chef-cookbooks/chef-ingredient'
 issues_url 'https://github.com/chef-cookbooks/chef-ingredient/issues'
-
-chef_version '>= 12.5'
+chef_version '>= 12.5' if respond_to?(:chef_version)
