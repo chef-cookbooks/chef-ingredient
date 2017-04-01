0883a98cd5c7aefc6ce84b83c849e0bade8b9d36
Nathan Cerny <ncerny@gmail.com>
2017-04-01 11:48:20 -0500

    Fix lint failures.
    Signed-off-by: Nathan Cerny <ncerny@gmail.com>

--- Berksfile
+++ Berksfile
@@ -5,6 +5,5 @@ metadata
 group :test do
   cookbook 'chef-services',
            '>= 4.0.0',
-           git: 'https://github.com/stephenlauck/chef-services.git',
-           branch: 'ad/simplify_pkgs'
+           git: 'https://github.com/stephenlauck/chef-services.git'
 end
--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,3 @@
-# rubocop:disable LineLength
 name 'chef_stack'
 maintainer 'Chef Software Inc'
 maintainer_email 'success@chef.io'
@@ -7,6 +6,6 @@ description 'Resource Cookbook for Managing Chef Software Inc Product Suite'
 long_description 'Resource Cookbook for Managing Chef Software Inc Product Suite'
 issues_url 'https://github.com/ncerny/chef_stack/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/ncerny/chef_stack' if respond_to?(:source_url)
-version '0.6.1'
+version '0.6.2'
 
 depends 'chef-ingredient'
