e0ddbb5c397a63866c78f947c4849feee47d2a5d
Aaron Lane <aaron.lane@newcontext.com>
2017-04-14 10:28:53 -0400

    Pessimistically pin mixlib-install to 3.2
    Signed-off-by: Aaron Lane <aaron.lane@newcontext.com>

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -87,7 +87,7 @@ module ChefIngredientCookbook
             'mixlib-install'
           )
         else
-          install_gem_from_rubygems('mixlib-install', '~> 3')
+          install_gem_from_rubygems('mixlib-install', '~> 3.2')
         end
 
         require 'mixlib/install'
