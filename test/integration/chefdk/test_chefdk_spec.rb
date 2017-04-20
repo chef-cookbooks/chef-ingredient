a64ff440a8b51566ecc2551ba954ce9680727117
Patrick Wright <patrick@chef.io>
2017-04-19 17:28:03 -0700

    add chef 12 kitchen tests
    Signed-off-by: Patrick Wright <patrick@chef.io>

--- test/integration/chefdk/test_chefdk_spec.rb
+++ test/integration/chefdk/test_chefdk_spec.rb
@@ -1,4 +1,17 @@
-resource_command = os.windows? ? 'powershell' : 'command'
+# The way this spec is written is more of an experiment. Executing Windows programs for the first time
+# via inspec require the fully qualified path. Even though the bin has been added to $env:Path the session
+# has not been reloaded. This reloads $env:Path before calling the program. I could see this being beneficial to
+# inspec's default behavior.
+
+resource_command = 'command'
+
+if os.windows?
+  resource_command = 'powershell'
+
+  describe powershell('$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")') do
+    its('exit_status') { should eq 0 }
+  end
+end
 
 describe send(resource_command, 'chef --version') do
   its('stdout') { should match /Chef Development Kit Version: 1.2.22/ }
