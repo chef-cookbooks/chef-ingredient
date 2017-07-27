302e7813aec9185669d6ce36748a74f44c3c3d64
Tim Smith <tsmith@chef.io>
2017-07-27 15:52:50 -0700

    Add supported platforms to the metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,10 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Primitives for managing Chef products and packages'
 
+%w(amazon centos redhat scientific oracle fedora debian ubuntu).each do |os|
+  supports os
+end
+
 source_url 'https://github.com/chef-cookbooks/chef-ingredient'
 issues_url 'https://github.com/chef-cookbooks/chef-ingredient/issues'
 chef_version '>= 12.7' if respond_to?(:chef_version)
