d15aea7678242310259b9b2e424b085884ed31fd
Tim Smith <tsmith@chef.io>
2017-07-27 15:47:46 -0700

    Update comment headers and copyrights
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 #
 # Author:: Salim Afiune <afiune@chef.io>
-# Copyright (c) 2015-2016, Chef Software, Inc. <legal@chef.io>
+# Copyright:: 2015-2017, Chef Software, Inc. <legal@chef.io>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- spec/unit/chef_file_spec.rb
+++ spec/unit/chef_file_spec.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: chef_stack
+# Cookbook:: chef-ingredient
 # Spec:: chef_file
 #
 # Copyright 2016 Chef Software Inc
