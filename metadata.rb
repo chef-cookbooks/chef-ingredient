6002aad2fdf3282b90cf4caa3d212f786713faa7
Tim Smith <tsmith@chef.io>
2017-07-27 15:51:48 -0700

    Add the missing version bump in the metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -7,4 +7,4 @@ description 'Primitives for managing Chef products and packages'
 
 source_url 'https://github.com/chef-cookbooks/chef-ingredient'
 issues_url 'https://github.com/chef-cookbooks/chef-ingredient/issues'
-chef_version '>= 12.5' if respond_to?(:chef_version)
+chef_version '>= 12.7' if respond_to?(:chef_version)
