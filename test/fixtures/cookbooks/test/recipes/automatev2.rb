becf28d7b6486aa792545ba7fd7f87a6611e121e
Corey Hemminger <hemminger@hotmail.com>
2018-12-17 16:40:37 -0600

    fix config value
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- test/fixtures/cookbooks/test/recipes/automatev2.rb
+++ test/fixtures/cookbooks/test/recipes/automatev2.rb
@@ -2,7 +2,7 @@
 chef_automatev2 'automatev2.chefstack.local' do
   config <<~EOS
     [global.v1]
-      fqdn = "chef-server.example.com"
+      fqdn = "chef-automate.example.com"
   EOS
   accept_license true
 end
