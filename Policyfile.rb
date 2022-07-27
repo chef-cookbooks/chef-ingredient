6fd1dbdc67dbcdde19d03391bca5d83793be8c27
Corey Hemminger <hemminger@hotmail.com>
2022-07-27 16:54:49 -0500

    test
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- Policyfile.rb
+++ Policyfile.rb
@@ -13,12 +13,12 @@ default_source :supermarket
 run_list 'chef-ingredient::default'
 named_run_list :test, 'test'
 named_run_list :test_install_git, 'test::install_git'
-named_run_list :test_repo, 'test::repo'
-named_run_list :test_local, 'test::local'
-named_run_list :test_chef_workstation, 'test::chef_workstation'
-named_run_list :test_inspec, 'test::inspec'
-named_run_list :test_chef_server, 'test::chef_server'
-named_run_list :test_chef_automatev2, 'test::automatev2'
+named_run_list :test_repo, %w(test::install_git test test::repo)
+named_run_list :test_local, %w(test::install_git test test::local)
+named_run_list :test_chef_workstation, %w(test::install_git test test::chef_workstation)
+named_run_list :test_inspec, %w(test::install_git test test::inspec)
+named_run_list :test_chef_server, %w(test::install_git test test::chef_server)
+named_run_list :test_chef_automatev2, %w(test::install_git test test::automatev2)
 
 # Specify a custom source for a single cookbook:
 cookbook 'chef-ingredient', path: '.'
